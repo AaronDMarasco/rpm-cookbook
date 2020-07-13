04817901079544d9121b65aada1164d07e462ec4
Aaron D. Marasco <rpm-cookbook@marascos.net>
2020-07-13 15:43:43 -0400

    A little cleanup

--- build/travis.sh
+++ build/travis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -ex
-# openssl aes-256-cbc -v -k "${travis_key_password}" -d -md sha256 -a -in .travis_key.enc -out travis_key
-chmod 400 travis_key
+set -e
 echo "Host github.com" > ~/.ssh/config
 echo "  IdentityFile $(pwd)/travis_key" >> ~/.ssh/config
+chmod 400 travis_key
+set -x
 git remote set-url origin git@github.com:AaronDMarasco/rpm-cookbook.git
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 if ! git diff --exit-code README.md; then
